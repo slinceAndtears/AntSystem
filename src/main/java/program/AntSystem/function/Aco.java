6e2061486a26265ef684e8e8c5cf8c18665b928f
slienceAndtears <623034792@qq.com>
2022-02-23 13:45:16 +0800

    update new dataset 3340 nodes

--- src/main/java/program/AntSystem/function/Aco.java
+++ src/main/java/program/AntSystem/function/Aco.java
@@ -614,12 +614,16 @@ public class Aco {
         }
     }
 
-    static void detectNodeLink(){
-        for (int i=1;i<=2088;++i){
-            if (!allGraph.vertex.keySet().contains(i)){
+    static void detectNodeLink() {
+        int nodeSum = 3340;
+        int sum = 0;
+        for (int i = 1; i <= nodeSum; ++i) {
+            if (!allGraph.vertex.keySet().contains(i)) {
+                ++sum;
                 System.out.println(i);
             }
         }
+        logger.info("guer Node sum is {}", sum);
     }
 
     public static void main(String[] args) throws IOException {
