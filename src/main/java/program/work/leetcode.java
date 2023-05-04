41225c69c61521beb94d949b72874fdf1642e304
slienceAndtears <623034792@qq.com>
2023-05-04 22:01:44 +0800

    invertTree

--- src/main/java/program/work/leetcode.java
+++ src/main/java/program/work/leetcode.java
@@ -695,6 +695,18 @@ public class leetcode {
         return tail.next;
     }
 
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
     public static void main(String[] args) {
         System.out.println(groupAnagramsNew(new String[]{"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"}));
 
