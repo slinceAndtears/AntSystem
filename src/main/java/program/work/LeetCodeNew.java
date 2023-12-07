3589fe9fd656c4de98b7d5fe1e4ed8f7fce8cb82
slienceAndtears <623034792@qq.com>
2023-12-07 23:27:39 +0800

    2023-12-07

--- src/main/java/program/work/LeetCodeNew.java
+++ src/main/java/program/work/LeetCodeNew.java
@@ -480,6 +480,22 @@ public class LeetCodeNew {
         return low;
     }
 
+    public int lengthOfLIS(int[] nums) {
+        int res = 0;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < nums.length; ++i) {
+            int max = 0;
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    max = Math.max(max, dp[j]);
+                }
+            }
+            dp[i] = max + 1;
+            res = Math.max(dp[i], res);
+        }
+        return res;
+    }
 
     public static void main(String[] args) {
         isPalindrome("ab2a");
