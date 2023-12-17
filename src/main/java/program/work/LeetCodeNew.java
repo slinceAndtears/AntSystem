4fcb7b113fc5ec859af475e156215f23df1af3a4
slienceAndtears <623034792@qq.com>
2023-12-17 22:50:46 +0800

    2023-12-17

--- src/main/java/program/work/LeetCodeNew.java
+++ src/main/java/program/work/LeetCodeNew.java
@@ -831,6 +831,17 @@ public class LeetCodeNew {
         return tail.next;
     }
 
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.val == targetSum && root.left == null && root.right == null) {
+            return true;
+        }
+        return hasPathSum(root.left, targetSum - root.val) ||
+                hasPathSum(root.right, targetSum - root.val);
+    }
+
     public static void main(String[] args) {
 
     }
