df3df6e2ebfa6ed69823f12a8608119b11d21d30
slienceAndtears <623034792@qq.com>
2024-01-01 23:35:34 +0800

    2024-01-01

--- src/main/java/program/work/LeetCodeNew.java
+++ src/main/java/program/work/LeetCodeNew.java
@@ -1175,6 +1175,39 @@ public class LeetCodeNew {
         }
     }
 
+    public int findPeakElement(int[] nums) {
+        if (nums.length == 1) {
+            return 0;
+        }
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (mid == 0) {
+                if (nums[mid] > nums[mid + 1]) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+            } else if (mid == nums.length - 1) {
+                if (nums[mid] > nums[mid - 1]) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                    return mid;
+                } else if (nums[mid] > nums[mid - 1] && nums[mid] < nums[mid + 1]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return low;
+    }
+
     public static void main(String[] args) {
         String s = simplifyPath("/../..ga/b/.f..d/..../e.baaeeh./.a");
         System.out.println(s);
