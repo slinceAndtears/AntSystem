7053c22737b1147a8698a60b4c7f5b0913762915
slienceAndtears <623034792@qq.com>
2024-01-22 23:00:05 +0800

    2024-01-22

--- src/main/java/program/work/LeetCodeNew.java
+++ src/main/java/program/work/LeetCodeNew.java
@@ -1548,6 +1548,33 @@ public class LeetCodeNew {
         return res;
     }
 
+    public void setZeroes(int[][] matrix) {
+        int[] x = new int[matrix.length];
+        int[] y = new int[matrix[0].length];
+        for (int i = 0; i < matrix.length; ++i) {
+            for (int j = 0; j < matrix[i].length; ++j) {
+                if (matrix[i][j] == 0) {
+                    x[i] = 1;
+                    y[j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < x.length; ++i) {
+            if (x[i] == 1) {
+                for (int j = 0; j < matrix[i].length; ++j) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (int j = 0; j < y.length; ++j) {
+            if (y[j] == 1) {
+                for (int i = 0; i < matrix.length; ++i) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         trailingZeroes(15);
     }
