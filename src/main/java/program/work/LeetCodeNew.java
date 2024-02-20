c13988817120d10e4258aff9bbb90646e2b7380f
slienceAndtears <623034792@qq.com>
2024-02-20 22:35:24 +0800

    2024-02-20

--- src/main/java/program/work/LeetCodeNew.java
+++ src/main/java/program/work/LeetCodeNew.java
@@ -1772,6 +1772,44 @@ public class LeetCodeNew {
         return res;
     }
 
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        boolean tag = false;
+        for (int i = 0; i < m; ++i) {
+            if (obstacleGrid[i][0] == 1) {
+                tag = true;
+            }
+            if (tag) {
+                dp[i][0] = 0;
+            } else {
+                dp[i][0] = 1;
+            }
+        }
+        tag = false;
+        for (int i = 0; i < n; ++i) {
+            if (obstacleGrid[0][i] == 1) {
+                tag = true;
+            }
+            if (tag) {
+                dp[0][i] = 0;
+            } else {
+                dp[0][i] = 1;
+            }
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
     public static void main(String[] args) {
         coinChange(new int[]{1}, 1);
     }
