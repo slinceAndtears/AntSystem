4bf12202f553e8d89af2690545e6145b8ef03d10
slienceAndtears <623034792@qq.com>
2024-02-24 23:12:24 +0800

    2024-02-24

--- src/main/java/program/work/LeetCodeNew.java
+++ src/main/java/program/work/LeetCodeNew.java
@@ -1865,6 +1865,36 @@ public class LeetCodeNew {
         return 0;
     }
 
+    public void change(char[][] board, int i, int j) {
+        if (i < 0 || j < 0 || i == board.length || j == board[i].length || board[i][j] != 'O') {
+            return;
+        }
+        board[i][j] = 'A';
+        change(board, i + 1, j);
+        change(board, i - 1, j);
+        change(board, i, j + 1);
+        change(board, i, j - 1);
+    }
+
+    public void solve(char[][] board) {
+        for (int i = 0; i < board.length; ++i) {
+            change(board, i, 0);
+            change(board, i, board[i].length - 1);
+        }
+        for (int i = 0; i < board[0].length; ++i) {
+            change(board, 0, i);
+            change(board, board.length - 1, i);
+        }
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[i].length; ++j) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == 'A') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
         coinChange(new int[]{1}, 1);
