c53f2fd443cb48233c6713f5582404e68eb2bf26
slienceAndtears <623034792@qq.com>
2024-02-25 22:33:01 +0800

    2024-02-25

--- src/main/java/program/work/LeetCodeNew.java
+++ src/main/java/program/work/LeetCodeNew.java
@@ -1896,6 +1896,50 @@ public class LeetCodeNew {
         }
     }
 
+    /**
+     * ho -> ro = h r
+     *  min ([i-1,j-1],[i-1,j],[i,j-1]) + 1
+     *    h o r s e
+     * r  1 2 2 3 4
+     * o  2 1 2
+     * s  3
+     * */
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        // 有一个字符串为空串
+        if (n * m == 0) {
+            return n + m;
+        }
+
+        // DP 数组
+        int[][] D = new int[n + 1][m + 1];
+
+        // 边界状态初始化
+        for (int i = 0; i < n + 1; i++) {
+            D[i][0] = i;
+        }
+        for (int j = 0; j < m + 1; j++) {
+            D[0][j] = j;
+        }
+
+        // 计算所有 DP 值
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                int left = D[i - 1][j] + 1;
+                int down = D[i][j - 1] + 1;
+                int left_down = D[i - 1][j - 1];
+                if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
+                    left_down += 1;
+                }
+                D[i][j] = Math.min(left, Math.min(down, left_down));
+            }
+        }
+        return D[n][m];
+
+    }
+
     public static void main(String[] args) {
         coinChange(new int[]{1}, 1);
     }
