d78796665f988cf68ddc8d31e096b39148964aa1
slienceAndtears <623034792@qq.com>
2024-02-27 22:38:51 +0800

    2024-02-27

--- src/main/java/program/work/LeetCodeNew.java
+++ src/main/java/program/work/LeetCodeNew.java
@@ -1958,6 +1958,21 @@ public class LeetCodeNew {
         return res;
     }
 
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         coinChange(new int[]{1}, 1);
     }
