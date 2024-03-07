bbca287fe4dd6494311f9e58ce2cd814782eff95
slienceAndtears <623034792@qq.com>
2024-03-07 22:39:30 +0800

    2024-03-07

--- src/main/java/program/work/LeetCodeNew.java
+++ src/main/java/program/work/LeetCodeNew.java
@@ -2043,6 +2043,42 @@ public class LeetCodeNew {
         return res;
     }
 
+    public int maximalSquare(char[][] matrix) {
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        int max = 0;
+        for (int i = 0; i < matrix.length; ++i) {
+            for (int j = 0; j < matrix[i].length; ++j) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    }
+                }
+                max = Math.max(max, dp[i][j]);
+            }
+        }
+        return max * max;
+    }
+
+    public int countSquares(int[][] matrix) {
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        int sum = 0;
+        for (int i = 0; i < matrix.length; ++i) {
+            for (int j = 0; j < matrix[i].length; ++j) {
+                if (matrix[i][j] == 1) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j]=1;
+                    } else {
+                        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    }
+                }
+                sum += dp[i][j];
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         coinChange(new int[]{1}, 1);
     }
